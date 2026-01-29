9f27cad54d9ad7ac30bb0ec10330935bd2e8d5ff
iachb <ivanchavezbrb@gmail.com>
2026-01-29 17:43:08 -0600

    Mapping specification feature implementation

---
+++ backend/src/Core/Ecommerce.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Ecommerce.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
---
+++ backend/src/Infrastructure/Specification/SpecificationEvaluator.cs
@@ -0,0 +1,41 @@
+﻿using Ecommerce.Application.Contracts.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Specification
+{
+    public class SpecificationEvaluator<T> where T : class
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> spec)
+        {
+            var query = inputQuery;
+
+            // Apply criteria
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            // Apply sorting
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            else if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            // Apply paging
+            if (spec.isPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
+            // Apply includes
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include)).AsSplitQuery().AsNoTracking();
+
+            return query;
+        }
+    }
+}
