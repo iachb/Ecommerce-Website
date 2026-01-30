c5c219bd39ef876ca155ce0a7b4c3e33767b917e
iachb <ivanchavezbrb@gmail.com>
2026-01-29 18:34:53 -0600

    Get products using MediatR query and query handler

---
+++ backend/src/Api/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Application.Features.Products.Queries.GetProductList;
+using Ecommerce.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Ecommerce.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("list", Name = "GetProductList")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductList()
+        {
+            var query = new GetProductListQuery();
+            var products = await _mediator.Send(query);
+            return Ok(products);
+        }
+    }
+}
---
+++ backend/src/Core/Ecommerce.Application/Features/Products/Queries/GetProductList/GetProductListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Application.Persistence;
+using Ecommerce.Domain;
+using MediatR;
+using System.Linq.Expressions;
+
+namespace Ecommerce.Application.Features.Products.Queries.GetProductList
+{
+    public class GetProductListQueryHandler : IRequestHandler<GetProductListQuery, List<Product>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetProductListQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<List<Product>> Handle(GetProductListQuery request, CancellationToken cancellationToken)
+        {
+            var includes = new List<Expression<Func<Product, object>>>
+            {
+                p => p.Images!,
+                p => p.Reviews!
+            };
+
+            var products = await _unitOfWork.Repository<Product>().GetAsync(
+                predicate: null,
+                orderBy: x => x.OrderBy(y => y.Name),
+                includes: includes,
+                disableTracking: false
+            );
+
+            return new List<Product>(products);
+        }
+    }
+}
