81911f60ebbf303f2f2a791a467e6ab6644068e4
iachb <ivanchavezbrb@gmail.com>
2026-03-17 19:49:51 -0600

    Adding validation for registering new users

--- backend/src/Core/Ecommerce.Application/Features/Auths/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
+++ backend/src/Core/Ecommerce.Application/Features/Auths/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace Ecommerce.Application.Features.Auths.Users.Commands.RegisterUser
 {
-    public class RegisterUserCommandValidator
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
     {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Surname)
+                .NotEmpty().WithMessage("Surname is required.");
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required.");
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.");
+        }
     }
 }
