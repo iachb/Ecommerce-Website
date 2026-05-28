e2b9152883b9e80bced3b525f1ee72b4610b609f
iachb <ivanchavezbrb@gmail.com>
2026-05-27 18:18:31 -0600

    Changing "Department" to "State" in database columns

---
+++ backend/src/Infrastructure/Migrations/20260528000532_RenameDepartmentToState.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDepartmentToState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Department",
+                table: "Addresses",
+                newName: "State");
+
+            migrationBuilder.RenameColumn(
+                name: "Department",
+                table: "OrderAddresses",
+                newName: "State");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "Addresses",
+                newName: "Department");
+
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "OrderAddresses",
+                newName: "Department");
+        }
+    }
+}
