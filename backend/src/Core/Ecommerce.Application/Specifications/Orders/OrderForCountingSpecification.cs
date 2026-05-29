58c9ab97d997281be444bdae7e61af92a0404a75
iachb <ivanchavezbrb@gmail.com>
2026-05-28 19:15:28 -0600

    Specification for orders implementation

---
+++ backend/src/Core/Ecommerce.Application/Specifications/Orders/OrderForCountingSpecification.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain;
+
+namespace Ecommerce.Application.Specifications.Orders
+{
+    public class OrderForCountingSpecification : BaseSpecification<Order>
+    {
+        public OrderForCountingSpecification(OrderSpecificationParams orderParams)
+            : base(x =>
+                (string.IsNullOrEmpty(orderParams.Username) || x.BuyerUsername!.Contains(orderParams.Username)) &&
+                (!orderParams.Id.HasValue || x.Id == orderParams.Id))
+        {
+        }
+    }
+}
---
+++ backend/src/Core/Ecommerce.Application/Specifications/Orders/OrderSpecificationParams.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Application.Specifications.Orders
+{
+    public class OrderSpecificationParams : SpecificationsParams
+    {
+        public string? Username { get; set; }
+        public int? Id { get; set; }
+    }
+}
