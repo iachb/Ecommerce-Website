83c721526170dc0ca7a61274c49ea1d667876a53
iachb <ivanchavezbrb@gmail.com>
2026-06-22 21:39:28 -0600

    Remove delayed timout

--- frontend/src/actions/productAction.jsx
+++ frontend/src/actions/productAction.jsx
@@ -6,7 +6,6 @@ export const getProducts = createAsyncThunk(
   "products/getProducts",
   async (ThunkApi, { rejectWithValue }) => {
     try {
-      await delayedTimeout(1000); // Simulate a delay of 1 second
       return await axios.get(`/api/v1/Product/list`);
     } catch (err) {
       return rejectWithValue(`Errors: ${err.message}`);
