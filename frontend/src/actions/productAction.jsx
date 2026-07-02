4a3f2198ffb92e9f621b4ab601ed152857f594f4
iachb <ivanchavezbrb@gmail.com>
2026-07-01 22:29:53 -0600

    Pagination action and reducer

--- frontend/src/actions/productAction.jsx
+++ frontend/src/actions/productAction.jsx
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from '../utilities/axios';
+import axios from "../utilities/axios";
 import { delayedTimeout } from "../utilities/delayedTimeout";
+import { httpParams } from "../utilities/httpParams";
 
 export const getProducts = createAsyncThunk(
   "products/getProducts",
@@ -18,6 +19,20 @@ export const getProductById = createAsyncThunk(
   async (id, { rejectWithValue }) => {
     try {
       return await axios.get(`/api/v1/Product/${id}`);
+    } catch (err) {
+      return rejectWithValue(`Errors: ${err.message}`);
+    }
+  },
+);
+
+export const getProductPagination = createAsyncThunk(
+  "products/getProductPagination",
+  async (params, { rejectWithValue }) => {
+    try {
+      params = httpParams(params);
+      const paramUrl = new URLSearchParams(params).toString();
+      var results = await axios.get(`/api/v1/Product/pagination?${paramUrl}`);
+      return (await results).data;
     } catch (err) {
       return rejectWithValue(`Errors: ${err.message}`);
     }
---
+++ frontend/src/slices/productPaginationSlice.jsx
@@ -0,0 +1,81 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getProductPagination } from "../actions/productAction";
+
+export const initialState = {
+  products: [],
+  count: 0,
+  index: 1,
+  pageSize: 10,
+  pageCount: 0,
+  loading: false,
+  resultByPage: 0,
+  error: null,
+  search: null,
+  maxPrice: null,
+  minPrice: null,
+  category: null,
+  rating: null,
+};
+
+export const productPaginationSlice = createSlice({
+  name: "productPagination",
+  initialState,
+  reducers: {
+    searchPagination: (state, action) => {
+      state.search = action.payload.search;
+      state.pageIndex = 1;
+    },
+    setPageIndex: (state, action) => {
+      state.index = action.payload.index;
+    },
+    updatePrice: (state, action) => {
+      state.minPrice = action.payload.price[0];
+      state.maxPrice = action.payload.price[1];
+    },
+    resetPagination: (state, action) => {
+      state.search = null;
+      state.minPrice = null;
+      state.maxPrice = null;
+      state.category = null;
+      state.rating = null;
+      state.pageIndex = 1;
+    },
+    updateCategory: (state, action) => {
+      state.category = action.payload.category;
+    },
+    updateRating: (state, action) => {
+      state.rating = action.payload.rating;
+    },
+  },
+  extraReducers: {
+    [getProductPagination.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [getProductPagination.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.products = payload.data;
+      state.count = payload.count;
+      state.pageIndex = payload.pageIndex;
+      state.pageSize = payload.pageSize;
+      state.pageCount = payload.pageCount;
+      state.resultByPage = payload.resultByPage;
+      state.error = null;
+    },
+    [getProductPagination.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  searchPagination,
+  setPageIndex,
+  updatePrice,
+  resetPagination,
+  updateCategory,
+  updateRating,
+} = productPaginationSlice.actions;
+
+export const productPaginationReducer = productPaginationSlice.reducer;
